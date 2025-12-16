9021de3c8ab86d153235d3e083b8acbafad8a30a
Alexd32 <dementev.aleksei@gmail.com>
2025-12-16 19:30:30 +0300

    v1

---
+++ popup/popup.js
@@ -0,0 +1,8 @@
+(() => {
+  const btn_start = document.getElementById('btn_start');
+
+  btn_start.addEventListener('click', () => {
+    // пока просто заглушка
+    console.log('Start clicked');
+  });
+})();
